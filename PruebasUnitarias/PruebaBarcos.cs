9a2c6b6db9a898ea8d6e6901670da1ae6f941367
Julian <julianmartinez2797@gmail.com>
2018-11-25 20:06:29 -0300

    Agrego el metodo ganar enfrentamiento

--- PruebasUnitarias/PruebaBarcos.cs
+++ PruebasUnitarias/PruebaBarcos.cs
@@ -8,25 +8,51 @@ namespace PruebasUnitarias
     [TestClass]
     public class PruebaBarcos
     {
+        Guerrero unGuerrero;
+        Navegador unNavegador;
+        Cocinero unCocinero;
+        JackSparrow jackSparrow;
+        ArmadaInglesa armadaInglesa;
+        Barco unBarco;
+        Barco otroBarco;
+
+        [TestCleanup]
+        public void testClean()
+        {
+            unGuerrero = null;
+            unNavegador = null;
+            unCocinero = null;
+            jackSparrow = null;
+            armadaInglesa = null;
+            unBarco = null;
+            otroBarco = null;
+        }
+
+        [TestInitialize]
+        public void testInit()
+        {
+            unGuerrero = new Guerrero(150, 5, 10);
+            unNavegador = new Navegador(5, 10);
+            List<String> ingredientes = new List<String>();
+            ingredientes.Add("tomate");
+            ingredientes.Add("arroz");
+            ingredientes.Add("carne");
+            unCocinero = new Cocinero(100, 10, ingredientes);
+            jackSparrow = JackSparrow.obtenerInstancia();
+            List<ITripulante> tripulantes = new List<ITripulante>();
+            tripulantes.Add(unGuerrero);
+            tripulantes.Add(unCocinero);
+            tripulantes.Add(unNavegador);
+            armadaInglesa = ArmadaInglesa.obtenerInstancia();
+            unBarco = new Barco(50, 100, 10, tripulantes, armadaInglesa);
+            otroBarco = new Barco(80, 40, 40, tripulantes, armadaInglesa);
+        }
+
         [TestMethod]
         // Testeando: "El capitan de un barco es el tripulante con mayor poder de mando"
         public void CapitanDeUnBarco()
         {
             //Arrange
-            var unGuerrero = new Guerrero(150, 5, 10);
-            List<String> ingredientes = new List<String>();
-            ingredientes.Add("tomate");
-            ingredientes.Add("arroz");
-            ingredientes.Add("carne");
-            var unCocinero = new Cocinero(100, 10, ingredientes);
-            var unNavegador = new Navegador(5, 10);
-            List<ITripulante> tripulantes = new List<ITripulante>();
-            tripulantes.Add(unGuerrero);
-            tripulantes.Add(unCocinero);
-            tripulantes.Add(unNavegador);
-            var armadaInglesa = ArmadaInglesa.obtenerInstancia();
-            var jackSparrow = JackSparrow.obtenerInstancia();
-            var unBarco = new Barco(50, 100, 50, tripulantes, armadaInglesa);
             //Act
             var resultado = unBarco.capitan();
             //Assert
@@ -39,24 +65,42 @@ namespace PruebasUnitarias
         public void FuerzaDeBarco()
         {
             //Arrange
-            var unGuerrero = new Guerrero(150, 5, 10);
-            List<String> ingredientes = new List<String>();
-            ingredientes.Add("tomate");
-            ingredientes.Add("arroz");
-            ingredientes.Add("carne");
-            var unCocinero = new Cocinero(100, 10, ingredientes);
-            var unNavegador = new Navegador(5, 10);
-            List<ITripulante> tripulantes = new List<ITripulante>();
-            tripulantes.Add(unGuerrero);
-            tripulantes.Add(unCocinero);
-            tripulantes.Add(unNavegador);
-            var armadaInglesa = ArmadaInglesa.obtenerInstancia();
-            var jackSparrow = JackSparrow.obtenerInstancia();
-            var unBarco = new Barco(50, 100, 50, tripulantes, armadaInglesa);
             //Act
             var resultado = unBarco.fuerza();
             //Assert
             Assert.AreEqual(180, resultado);
         }
+        [TestMethod]
+        // Testeando: "Un barco consigue el bonus del bando al cual pertenece"
+        public void BarcoObtieneBonus()
+        {
+            //Arrange
+            //Act
+            armadaInglesa.aplicarBonus(unBarco);
+            var resultado = unBarco.municiones;
+            //Assert
+            Assert.AreEqual(13, resultado);
+        }
+        [TestMethod]
+        // Testeando: "Cuando un barco pierde un enfrentamiento, queda desolado"
+        public void BarcoPierdeEnfrentamiento()
+        {
+            //Arrange
+            //Act
+            unBarco.perderEnfrentamiento();
+            var resultado = unBarco.estaDesolado();
+            //Assert
+            Assert.IsTrue(resultado);
+        }
+        [TestMethod]
+        /* Testeando: "Cuando un barco intenta atacar a otro barco 
+        con mas municiones de las que posee, no puede hacerlo"*/
+        public void UnBarcoNoPuedeAtacarAOtroSinoTieneLasMunicionesSuficientes()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
     }
 }
